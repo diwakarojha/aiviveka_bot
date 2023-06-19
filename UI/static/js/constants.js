2153578d7cb99bc87c1f5588da4102b24af43cdd
diwakarojha <dojha00@gmail.com>
2023-06-19 16:15:16 +0530

    link modified

--- UI/static/js/constants.js
+++ UI/static/js/constants.js
@@ -1,6 +1,6 @@
 // const action_name = "utter_greet"; // also check code in chat.js
 const start_chat_utterance = "Hi";
 // Replace with Public IP address where Rasa server is hosted below
-const rasa_server_url = "https://510c-150-129-168-121.ngrok-free.app/webhooks/rest/webhook";
+const rasa_server_url = "https://0369-150-129-168-104.ngrok-free.app/webhooks/rest/webhook";
 // const rasa_server_url = "http://localhost:5005/webhooks/rest/webhook";
 const sender_id = uuidv4();
